3bd382a60bdc9d42bb4be0980f5aff6392daec86
buttabu <buttabu@yahoo.com>
2017-01-23 21:16:19 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 (function() {
   'use strict';
-  angular.module('LunchCheck', []);
-.controller('LunchCheckController', LunchCheckController){
-  LunchCheckController.$inject = ['$scope'];
-  function LunchCheckController ('$scope'){
-    $scope.input = "";
-  };
-
-};
+  angular.module('LunchCheck', [])
+.controller('LunchCheckController', LunchCheckController);
+  LunchCheckController.$inject = ['$scope',$filter];
+  function LunchCheckController ($scope, $filter){
+    $scope.menu = "";
+    $scope.checking_and_printing = function (){
+      if($scope.menu == "") {
+          $scope.message  = "Please Enter Data First....";
+          return;
+      }
+      var values_count  = $scope.menu.split(",").length;
+      if (values_count < 4) {
+        $scope.message  = " Let's Enjoy....";
+      }
+      else {
+        $scope.message  = " It's too much.....";
+      }
+    }
+  }
 })();
